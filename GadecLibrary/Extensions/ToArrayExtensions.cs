2b5dac88090534c3940fc70c054a0e2f3d1508f0
wilbird <wilbertholkema@outlook.com>
2025-02-15 12:01:52 +0100

    GCL-006: Added extension class

---
+++ GadecLibrary/Extensions/ToArrayExtensions.cs
@@ -0,0 +1,8 @@
+﻿using System.Data;
+
+namespace GadecLibrary.Extensions;
+public static class ToArrayExtensions
+{
+    public static DataRow[] ToArray(this DataRowCollection eCollection) => eCollection.Cast<DataRow>().ToArray();
+    public static DataColumn[] ToArray(this DataColumnCollection eCollection) => eCollection.Cast<DataColumn>().ToArray();
+}
