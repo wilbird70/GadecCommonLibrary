a51ce4a4868b060c7e5cb45be5d22c954fbb8878
wilbird <wilbertholkema@outlook.com>
2025-02-15 12:03:09 +0100

    GCL-007: Add empty form

---
+++ GadecFormsLibrary/Extensions/ToArrayExtensions.cs
@@ -0,0 +1,9 @@
+﻿using GadecFormsLibrary.Extensions;
+using System.Data;
+
+namespace GadecFormsLibrary.Extensions;
+public static class ToArrayExtensions
+{
+    public static Control[] ToArray(this Control.ControlCollection eCollection) => eCollection.Cast<Control>().ToArray();
+    public static DataGridViewRow[] ToArray(this DataGridViewRowCollection eCollection) => eCollection.Cast<DataGridViewRow>().ToArray();
+}
